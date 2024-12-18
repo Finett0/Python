3642589f825a6cef49831638bf40a33d61661688
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-18 14:24:55 -0300

    Add files via upload

---
+++ Basico/operadores.py
@@ -0,0 +1,25 @@
+# soma +
+soma = 1+1 # o resultado da operação vai ser armazenado na variavel 'soma'
+print("1 + 1 =", soma) 
+
+soma += 1 # tambem dá para somar dessa forma +=
+
+#subtração - 
+subtracao = 1-1
+print("1 - 1 =", subtracao)
+
+#multiplicação * 
+multiplicacao = 2 * 2 
+print("2 x 2 =",multiplicacao)
+
+#divisão / 
+divisao = 10 / 2  # o operador de divisão de uma barro só " / " retorna uma divisão com um numero de ponto flutuante
+print("10 / 2 =",divisao,type(divisao))
+ 
+divisaoo = 10 // 2 # já com o operador de duas barras " // " retorna o resultado da divisão com numero inteiro
+print("10 // 2 =",divisaoo,type(divisaoo))
+
+#Modulo % representa o resto da divisão
+modulo = 10  % 2
+print("10 % 2 =",modulo)
+
