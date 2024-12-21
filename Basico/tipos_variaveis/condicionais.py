e90e5fd625e6a3012b812a6339891fc1375893d4
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-21 15:22:52 -0300

    Update condicionais.py

--- Basico/tipos_variaveis/condicionais.py
+++ Basico/tipos_variaveis/condicionais.py
@@ -1,1 +1,18 @@
 
+"""
+Condicionais servem para verificar algo e executar caso for verdadeiro
+    if = se
+    elife = se não se 
+    else: se não 
+"""
+num = int(input('Digite um numero: '))
+
+if num % 2 == 0:
+    print(f'O numero {num} é par')
+else:
+    print(f'O numero {num} é impar')
+
+#Condicional em uma linha
+idade = int(input('Digite sua idade: '))
+mensagem = 'Pode tirar carteira de motorista' if idade >= 18 else 'Infelizmente não pode tirar!'
+print(mensagem)
