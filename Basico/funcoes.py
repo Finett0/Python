773fa7c3f4b4eb2a57266685c4723bd8e47ff76c
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-24 11:28:01 -0300

    Create funcoes.py

---
+++ Basico/funcoes.py
@@ -0,0 +1,19 @@
+def ola(seu_nome):
+    print(f'Olá {seu_nome}')
+
+ola('Erika')
+
+
+def quadrado(numero):
+    resultado = numero * numero
+    return resultado
+
+resultado_quadrado = quadrado(4)
+print(resultado_quadrado)
+
+
+def soma(numero1,numero2):
+    resultado = numero1 + numero2
+    print(resultado)
+
+soma(3,2)
