3f5becc232986b21f4b6e78e70bd12595d5f3f99
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-24 11:26:49 -0300

    Create while.py

---
+++ Basico/while.py
@@ -0,0 +1,6 @@
+#While é um bloco de codigo que vai se repetir até que uma condição seja atendida
+print("Exemplo de While: ")
+contador = 0 
+while contador < 10:
+    print(f'Contador: {contador}')
+    contador += 1
