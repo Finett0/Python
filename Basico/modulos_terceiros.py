6a4fe621db0921e3ad6294ec0826ab04b467bb28
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2024-12-28 08:14:59 -0300

    Update modulos_terceiros.py

--- Basico/modulos_terceiros.py
+++ Basico/modulos_terceiros.py
@@ -1,1 +1,8 @@
-#Modulos de terceiros são bibliotecas criadas por usuarios ou empresas 
+# abra o terminal e execute o codigo pip install nome_biblioteca
+
+import requests
+
+url = 'https://www.exemplo.com'
+response = requests.get(url)
+
+print(f'A Solicitação HTTP para url {url} retornou o status {response.status_code}')
