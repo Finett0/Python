753b6a002e6bdf3780cd742553851b31fe4310c6
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-02 14:08:43 -0300

    Update pilares_poo.py

--- intermediário/pilares_poo.py
+++ intermediário/pilares_poo.py
@@ -1,1 +1,17 @@
-#
+class Animal:
+    def __init__(self,nome):
+        self.nome = nome
+    
+    def andar(self):
+        print(f'O animal {self.nome} andou')
+
+    def emitir_som(self):
+        pass
+
+class Cachorro(Animal): #Exemplo de Herença
+    def emitir_som(self):
+        return "Au,au"
+
+class Gato(Animal):
+    def emitir_som(self):
+        return "Miau, Miau"
