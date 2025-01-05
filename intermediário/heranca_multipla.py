7e4635e20e776ce04514d7a8350dedfad6c586a3
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-05 12:16:37 -0300

    Update heranca_multipla.py

--- intermediário/heranca_multipla.py
+++ intermediário/heranca_multipla.py
@@ -1,1 +1,29 @@
+class Animal:
+    def __init__(self,nome):
+        self.nome = nome
+    
+    def emitir_som(self):
+        pass
 
+class Mamifero(Animal):
+    def amamentar(self):
+        return f"{self.nome} está amamentando."
+    
+class Ave(Animal):
+    def voar(self):
+        return f"{self.nome} está voando."
+
+class Morcego(Mamifero,Ave): # Herança multipla pois herda os atributos e metodos das duas classes
+    def emitir_som(self):
+        return "Morcegos emitem sons ultrassônicos"
+    
+morcego = Morcego(nome="Batman")
+
+print(f"Nome: {morcego.nome}")
+
+print(f"\nAcessando os medodos da classe Mamifero")
+print(morcego.amamentar())
+print(f"\nAcessando os medodos da classe Ave")
+print(morcego.voar())
+print(f"\nAcessando os medodos da classe Animal")
+print(morcego.emitir_som())
