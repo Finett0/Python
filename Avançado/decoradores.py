7027dd7669279bc591750f4588b3eaf86bd412be
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-05 12:40:45 -0300

    Update decoradores.py

--- Avançado/decoradores.py
+++ Avançado/decoradores.py
@@ -1,1 +1,13 @@
-#decoradores
+#Em resumo, Decoradores são funções que modificam a funcionalidade de outra função
+def meu_decorador(func):
+    def wrapper():
+        print("Antes da função ser chamada")
+        func()
+        print("Depois da função ser chamada")
+    return wrapper
+
+@meu_decorador
+def minha_funcao():
+    print("Minha função foi chamada")
+
+minha_funcao()
