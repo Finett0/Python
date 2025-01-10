421b1439e57fdeba4e6e8b8a149fef60fae97e2b
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-10 16:06:00 -0300

    Update app.py

--- Framework/Flask/app.py
+++ Framework/Flask/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask #Aqui estou importando Flask da biblioteca Flask
 
-app = Flask(__name__)
+#Criação da aplicação
+app = Flask(__name__) #A variavel app é uma instacia da classe Flask
 
-@app.route("/")
-def hello_world():
+#Definindo Rotas
+@app.route("/") #As rotas são as URLs que a aplicação web suporta. Aqui estão as rotas definidas:
+def hello_world():#O decorador @app.route("/") associa a URL raiz (/) à função hello_world.
     return "Hello World!"
 
 @app.route("/about")
 def about():
     return "Página Sobre"
 
+#Execução da Aplicação
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#O método app.run() inicia o servidor web do flask e o parâmetro debug=True ativa o modo de depuração
